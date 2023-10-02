76e2fbae76d7ae59e071cdd5a9236dcb97927967
Roosevelth <roosevelth.soares@ime.eb.br>
2023-10-02 11:26:34 -0300

    javadoc

--- src/App.java
+++ src/App.java
@@ -1,4 +1,20 @@
+/**
+ * Nome do programa: Drone++.
+ * 
+ * Objetivo do nosso programa: Mostrar atraves de uma interface de usuario um algoritmo classico de grafos do problema do caixeiro viajante.
+ * 
+ * Utilidade para o algoritmo: Esse algoritmo pode ser usado para implementar a inteligencia artificial de um Drone. 
+ * 
+ * @author Cosenza, Vieira, Laxe, Roosevelth.
+ */
+
 public class App {
+
+    /**
+     * Classe principal do programa com unico metodo sendo a main do programa
+     * @param args Parametro da funcao main, vetor de stings, porem nao eh necessario fornecer parametros
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         new ScreenUI();
     }
--- src/ScreenUI.java
+++ src/ScreenUI.java
@@ -5,10 +5,20 @@ import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
+/**
+ * Interface grafica do programa.
+ * 
+ */
+
 public class ScreenUI implements ActionListener {
     //Objeto input_text da classe JTextField, trata-se da caixa de texto da nossa tela de entrada/saida
     JTextField input_text;
-    //Construtor da nossa classe ScreenUI, nao recebe parametros, ao inicializar o construtor do objeto a interface ja eh inicializada
+    
+    /**
+     *Construtor da nossa classe ScreenUI, nao recebe parametros, ao inicializar o construtor do objeto a interface ja eh inicializada
+     * 
+     */
+    
     public ScreenUI(){
     //Objeto jf da classe JFrame: usado para criar nossa tela inicial
     JFrame jf = new JFrame("Drone++");
@@ -41,7 +51,13 @@ public class ScreenUI implements ActionListener {
     jb1.addActionListener(this::inputUI);
     jb2.addActionListener(this);
     }
-    //Metodo que sera invocado pelo ActionListener ao clicar no botao jb1, vao inicializar a tela de entrada e saida
+    
+    /**
+     * Metodo que sera invocado pelo ActionListener ao clicar no botao jb1, vao inicializar a tela de entrada e saida
+     * 
+     * @param actionEvent Refente aos objetos da classe ActionEvent,adiciona um evento no botão pelo ActionListener.
+     */
+    
     public void inputUI(ActionEvent actionEvent){
         JFrame jf2 = new JFrame("Drone++");
         jf2.setVisible(true);
@@ -59,25 +75,47 @@ public class ScreenUI implements ActionListener {
         jb4.addActionListener(this::outputMessage);
 
     }
-    //Metodo que calcula a distancia entre dois pontos:(X1,Y1) e (X2,Y2) no plano cartesiano
-    //Recebe como parametros 4 variaveis do tipo double x1,y1,x2,y2 e retorna uma variavel double que representa a distancia entre os dois pontos
+
+    /**
+     * Metodo que calcula a distancia entre dois pontos:(X1,Y1) e (X2,Y2) no plano cartesiano
+     * 
+     * @param x1 Coodenada x1
+     * @param y1 Coodenada y1
+     * @param x2 Coodenada x2
+     * @param y2 Coodenada y2
+     * @return Retorna a distancia entre os dois pontos.
+     */
+    
     public static double dist(double x1, double y1, double x2, double y2) {
         return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
-    //Metodo que sera invocado pelo ActionListener ao clicar no botao jb4
-    //Esse metodo vai rodar o nosso algoritmo do Travel Salesman Problem(TSP), o algoritmo do TSP recebe como entrada a string do input_text e gera uma string para nossa saida, e entao a saida eh printada como uma mensagem na tela
+    
+    /**
+     * Metodo que sera invocado pelo ActionListener ao clicar no botao jb4
+     * Esse metodo vai rodar o nosso algoritmo do Travel Salesman Problem(TSP).
+     * 
+     * @param actionEvent  Recebe como entrada a string do input_text e gera uma string para nossa saida, e entao a saida eh printada como uma mensagem na tela.
+     */
+    
     public void outputMessage(ActionEvent actionEvent) {
         //Metodo getText do objeto input_text da classe JTextField, retorna a string digitada na caixa de texto, nao recebe argumentos
         String str = new String(input_text.getText());
         String[] args=str.split("\\s+");
         int n = Integer.parseInt(args[0]);
         double x[] = new double[n], y[] = new double[n];
+        // Aqui fazemos o parsing do input, precisamos saber como interpretar os argumentos, feito com uma conta rápida.
         for(int i = 1; i < 2 * n + 1; i += 2) {
             x[(i - 1) / 2] = Double.parseDouble(args[i]);
             y[(i - 1) / 2] = Double.parseDouble(args[i + 1]);
         }
-        double dp[][] = new double[n][(1 << n)];
+        // declaração das variáveis do TSP
+        // dp -> melhor distância alcançada, utilizando o bitmask e a última coordenada alcançada.
+        // ex.: bitmask -> 0001011, última coordenada -> 1
+        // quer dizer que dp[1][11] = menor distância de se sair da origem, andar pelas coordenadas 0, 1, 3, e se ter a coordenada 1 como a última alcançada
+        // prv -> guardar a transição da dp
+        double dp[][] = new double[n][(1 << n)]; 
         int prv[][] = new int[n][(1 << n)];
+        // definição de critérios iniciais
         for(int i = 0; i < n; i++) {
             dp[i][(1 << i)] = dist(0, 0, x[i], y[i]);
             prv[i][(1 << i)] = -1;
@@ -86,7 +124,10 @@ public class ScreenUI implements ActionListener {
             for(int i = 0; i < n; i++) {
                 for(int j = 0; j < n; j++) {
                     if(i == j) continue;
+                    // faço a conferência de se o bit i está ativo e o j não
+                    // caso isso for verdade, posso pensar na transição de ir do i para o j
                     if(((mask >> i) & 1) == 1 && ((mask >> j) & 1) == 0) {
+                        // somente faço a transição se não tiver chegado naquele estado ou o valor naquele estado é pior do que posso alcançar agora
                         if(dp[j][(mask | (1 << j))] == 0 || dp[j][(mask | (1 << j))] > dp[i][mask] + dist(x[i], y[i], x[j], y[j])) {
                             dp[j][(mask | (1 << j))] = dp[i][mask] + dist(x[i], y[i], x[j], y[j]);
                             prv[j][(mask | (1 << j))] = i;
@@ -97,6 +138,7 @@ public class ScreenUI implements ActionListener {
         }
         double tmp = Double.MAX_VALUE;
         int id = -1;
+        // recuperção da resposta, precisamos também pensar na volta para a origem
         for(int i = 0; i < n; i++) {
             if(dp[i][(1 << n) - 1] + dist(0, 0, x[i], y[i]) < tmp) {
                 tmp = dp[i][(1 << n) - 1] + dist(0, 0, x[i], y[i]);
@@ -104,6 +146,7 @@ public class ScreenUI implements ActionListener {
             }
         }
         ArrayList<Integer> path = new ArrayList<Integer>();
+        // recuperação do caminho, dado que sabemos a última coordenada alcançada, de lá podemos ir removendo bit a bit para encontrar o caminho baseado na matriz prv
         int mask = (1 << n) - 1;
         path.add(-1);
         while(id != -1) {
@@ -113,6 +156,7 @@ public class ScreenUI implements ActionListener {
             id = nid;
         }
         path.add(-1);
+        // transformação da resposta encontrada em string para o programa.
         String output1 = new String(tmp + " is the total distance.");
         String output2 = new String("The order of the points:\n");
         for(int i = 0; i < path.size(); i++) {
@@ -126,7 +170,12 @@ public class ScreenUI implements ActionListener {
         JOptionPane.showMessageDialog(null,output1);
         JOptionPane.showMessageDialog(null,output2);
     }
-    //Metodo que sera invocado pelo ActionListener ao clicar no botao jb2, apenas gera uma mensagem na tela de guia para o usuario usar a inteface
+    
+    /**
+     * Metodo que sera invocado pelo ActionListener ao clicar no botao jb2, apenas gera uma mensagem na tela de guia para o usuario usar a inteface
+     * 
+     */
+    
     public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(null,"Please go to 'Set Input' and Enter the coordinates that the drone should reach.\nThen, our app will give you the shortest path passing through all the points");
     }
